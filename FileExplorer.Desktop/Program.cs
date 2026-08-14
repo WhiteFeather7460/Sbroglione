4d75cf68390ec2f3e9943e237487f57d5271174a
WhiteFeather <34483048+WhiteFeather7460@users.noreply.github.com>
2026-08-14 23:18:03 +0200

    feat(ui): add Projektanker icon packs and register FontAwesome provider

--- FileExplorer.Desktop/Program.cs
+++ FileExplorer.Desktop/Program.cs
@@ -2,6 +2,8 @@ using System;
 
 using Avalonia;
 using Avalonia.ReactiveUI;
+using Projektanker.Icons.Avalonia;
+using Projektanker.Icons.Avalonia.FontAwesome;
 
 namespace FileExplorer.Desktop;
 
@@ -16,9 +18,13 @@ sealed class Program
 
     // Avalonia configuration, don't remove; also used by visual designer.
     public static AppBuilder BuildAvaloniaApp()
-        => AppBuilder.Configure<App>()
+    {
+        IconProvider.Current.Register<FontAwesomeIconProvider>();
+
+        return AppBuilder.Configure<App>()
             .UsePlatformDetect()
             .WithInterFont()
             .LogToTrace()
             .UseReactiveUI();
+    }
 }
