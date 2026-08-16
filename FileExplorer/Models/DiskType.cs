172d0d96de166388f9e3eb5fdada68e9bd381846
WhiteFeather <34483048+WhiteFeather7460@users.noreply.github.com>
2026-08-17 00:16:05 +0200

    feat(copy): aggiungi DiskType e CopyParallelismResolver

---
+++ FileExplorer/Models/DiskType.cs
@@ -0,0 +1,9 @@
+namespace FileExplorer.Models;
+
+/// <summary>Tipo di disco fisico su cui risiede un percorso, per adattare il parallelismo di copia.</summary>
+public enum DiskType
+{
+    Unknown,
+    Ssd,
+    Hdd
+}
