3efea10917f7bcb0c7eea418e01dcb3a6831d4e6
WhiteFeather <34483048+WhiteFeather7460@users.noreply.github.com>
2026-08-20 19:09:03 +0200

    feat(nav): toggle e persistenza pannello laterale in MainWindowViewModel

--- FileExplorer/ViewModels/MainWindowViewModel.cs
+++ FileExplorer/ViewModels/MainWindowViewModel.cs
@@ -1,5 +1,33 @@
+using System.Reactive;
+using System.Threading.Tasks;
+using FileExplorer.Services;
+using ReactiveUI;
+
 namespace FileExplorer.ViewModels;
 
+/// <summary>Stato della shell: pannello di navigazione laterale espanso/collassato (persistito).</summary>
 public class MainWindowViewModel : ViewModelBase
 {
+    private bool _isNavExpanded;
+
+    public MainWindowViewModel()
+    {
+        _isNavExpanded = AppSettingsStore.Current.NavExpanded;
+        ToggleNavCommand = ReactiveCommand.CreateFromTask(ToggleNavAsync);
+    }
+
+    public bool IsNavExpanded
+    {
+        get => _isNavExpanded;
+        private set => this.RaiseAndSetIfChanged(ref _isNavExpanded, value);
+    }
+
+    public ReactiveCommand<Unit, Unit> ToggleNavCommand { get; }
+
+    public async Task ToggleNavAsync()
+    {
+        IsNavExpanded = !IsNavExpanded;
+        AppSettingsStore.Current.NavExpanded = IsNavExpanded;
+        await AppSettingsStore.SaveCurrentAsync().ConfigureAwait(false);
+    }
 }
